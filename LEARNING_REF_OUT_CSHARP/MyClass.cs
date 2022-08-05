c9f128f7c2295abdc23b5b87eb6ec41c4cec197f
ali azimzadeh <aliazimzadeh@gmail.com>
2022-08-05 09:00:01 +0430

    first commit

---
+++ LEARNING_REF_OUT_CSHARP/MyClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LEARNING_REF_OUT_CSHARP
+{
+    internal class MyClass
+    {
+        public void Swap(int a, int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+
+            System.Console.WriteLine
+                  (string.Format("a = {0} , b = {1}", a, b));
+        }
+
+        public void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+
+            System.Console.WriteLine
+                  (string.Format("a = {0} , b = {1}", a, b));
+        }
+
+
+        public int Sum_Multiply(int a, int b, out int c)
+        {
+            int sum = a + b;
+            
+            int multiply = a * b;
+
+            c = multiply;
+
+            return sum;
+        }
+
+
+        //public int Sum_Multiply(int a , int b,out int c)
+        //{
+        //    int sum = a + b;
+        //    int multiply = a * b;
+
+        //    return sum; // compiler error beacuse the parameter c must be assigned to value 
+        //}
+    }
+}
---
+++ LEARNING_REF_OUT_CSHARP/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LEARNING_REF_OUT_CSHARP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //************************************************
+            //Solution 1
+            //************************************************
+
+            //int x = 20;
+            //int y = 30;
+
+            //MyClass myClass = new MyClass();
+
+            //myClass.Swap(a: x, b: y);
+
+            //System.Console.WriteLine
+            //    (string.Format("x = {0} , y = {1}", x, y));
+
+            //************************************************
+            //Solution 1
+            //************************************************
+
+            //************************************************
+            //Solution 2
+            //************************************************
+            //int x = 20;
+            //int y = 30;
+
+            //MyClass myClass = new MyClass();
+
+            //myClass.Swap(a: ref x, b: ref y);
+
+            //System.Console.WriteLine
+            //    (string.Format("x = {0} , y = {1}", x, y));
+
+            //************************************************
+            //Solution 2
+            //************************************************
+
+            //************************************************
+            //Solution 3
+            //************************************************
+            //int x ; //in ref x must be assign a value
+            //int y = 30;
+
+            //MyClass myClass = new MyClass();
+
+            //myClass.Swap(a: ref x, b: ref y); //Compiler Error!
+
+            //System.Console.WriteLine
+            //    (string.Format("x = {0} , y = {1}", x, y));
+
+            //************************************************
+            //Solution 3
+            //************************************************
+
+
+            //************************************************
+            //Solution 4
+            //************************************************
+            //int x = 20;
+            //int y = 30;
+
+            //int multiply;
+            //int sum;
+
+            //MyClass myClass = new MyClass();
+
+            //sum = 
+            //    myClass.Sum_Multiply(a: x, b: y, c: out multiply);
+
+            //System.Console.WriteLine
+            //    (string.Format("Sum = {0} , and  Multiply = {1}", sum, multiply));
+
+            //************************************************
+            //Solution 4
+            //************************************************
+            System.Console.ReadKey();
+        }
+
+
+
+    }
+}
